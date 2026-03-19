89bac81af86b4de4dd4a6f45b6016e5f04a93a00
nawfelrahmani@hotmail.com <nawfelrahmani@hotmail.com>
2026-03-19 07:16:15 +0000

    We added our three tables and created a join between product and sales

---
+++ models/intermediate/int_sales_margin.sql
@@ -0,0 +1,18 @@
+WITH source AS (
+
+    select 
+        orders_id,
+        date_date,
+        revenue,
+        quantity,
+        ROUND(revenue - purchase_price,2) AS purchase_cost
+    from {{ ref('stg_gz_raw__product') }} AS product
+    INNER JOIN {{ ref('stg_gz_raw__sales') }} AS sales
+    ON product.products_id = sales.products_id)
+
+    SELECT 
+        *
+        , ROUND( revenue - purchase_cost ,2) AS margin 
+        FROM source
+
+
